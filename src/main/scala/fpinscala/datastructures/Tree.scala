433a7a3ef48fc1d86743a2c102c45a3b37e56806
Ming Jin <zejin@expedia.com>
2018-04-19 18:48:06 -0500

    Finished chapter 3.

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -3,3 +3,43 @@ package fpinscala.datastructures
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  // Exercise 3.25
+  def size[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + size(l) + size(r)
+    }
+
+  // Exercise 3.26
+  def maximum(t: Tree[Int]): Int =
+    t match {
+        case Leaf(v) => v
+        case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  // Exercise 3.27
+  def depth[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + (depth(l) max depth(r))
+    }
+
+  // Exercise 3.28
+  def map[A](t: Tree[A])(f: A => A): Tree[A] =
+    t match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+
+  // Exercise 3.29
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+      case Leaf(v) => f(v)
+      case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    }
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_ => 1)((l, r) => 1 + l + r)
+  def depthViaFold[A](t: Tree[A]): Int = fold(t)(_ => 1)((l, r) => 1 + (l max r))
+  def maxViaFold[A](t: Tree[Int]): Int = fold(t)(v => v)(_ max _)
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
+}
---
+++ src/test/scala/fpinscala/datastructures/TreeTest.scala
@@ -0,0 +1,30 @@
+package fpinscala.datastructures
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class TreeTest extends FlatSpec with Matchers{
+  def mockTree = Branch(Branch(Leaf(4), Branch(Leaf(1), Leaf(2))), Leaf(1))
+  it should "return the number of nodes from size" in
+    assert(Tree.size(mockTree) == 7)
+
+  it should "return the leaf with maximum value from maximum" in
+    assert(Tree.maximum(mockTree) == 4)
+
+  it should "return the depth of the given tree" in
+    assert(Tree.depth(mockTree) == 4)
+
+  it should "return a true with modified values" in
+    assert(Tree.map(mockTree)(a => a * 2) == Branch(Branch(Leaf(8), Branch(Leaf(2), Leaf(4))), Leaf(2)))
+
+  it should "return the number of nodes from sizeViaFold" in
+    assert(Tree.sizeViaFold(mockTree) == 7)
+
+  it should "return the depth of the given tree from depthViaFold" in
+    assert(Tree.depthViaFold(mockTree) == 4)
+
+  it should "return the leaf with maximum value from maxViaFold" in
+    assert(Tree.maxViaFold(mockTree) == 4)
+
+  it should "return a true with modified values from mapViaFold" in
+    assert(Tree.mapViaFold(mockTree)(a => a * 2) == Branch(Branch(Leaf(8), Branch(Leaf(2), Leaf(4))), Leaf(2)))
+}
