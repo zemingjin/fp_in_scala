eeea1b4e9cf19680ea5e637b8c7a311981f135e0
Ming Jin <zejin@expedia.com>
2018-04-26 18:00:04 -0500

    After 4.4

--- src/main/scala/chapter_4/Chapter4.scala
+++ src/main/scala/chapter_4/Chapter4.scala
@@ -1,10 +1,21 @@
 package chapter_4
 
-object Chapter4 {
-  def failingFn2(i: Int): Int = {
-    try {
-    val x = 42 + 5
-    x + ((throw new Exception("fail!")): Int) }
-  catch { case e: Exception => 43 }
+class Chapter4 {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double =
+    if (xs.isEmpty) onEmpty
+    else xs.sum / xs.length
+
+  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
+    val optAge: Option[Int] = Try(age.toInt)
+    val optTickets: Option[Int] = Try(numberOfSpeedingTickets.toInt)
+    insuranceRateQuote(optAge, optTickets)
   }
+
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch { case e: Exception => None }
 }
--- src/main/scala/chapter_4/Option.scala
+++ src/main/scala/chapter_4/Option.scala
@@ -1,66 +1,35 @@
 package chapter_4
 
-import scala.{Either => _, None => _, Option => _, Some => _}
-
 sealed trait Option[+A] {
-  case object None extends Option[Nothing]
-  case class Some[+A](get: A) extends Option[A]
-
-  // Exercise 4.1
   def map[B](f: A => B): Option[B] = this match {
     case None => None
     case Some(a) => Some(f(a))
   }
+
+  // Exercise 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a flatMap (aa => b map (bb => f(aa, bb)))
+
+  // Exercise 4.1
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
+
   def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
-  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
-    case None => ob
-    case Some(_) => this
-  }
-  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
-  def filter(f: A => Boolean): Option[A] = this match {
-    case None => None
-    case Some(a) => if (f(a)) Some(a) else None
-  }
 
-  def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
-  // Exercise 4.2
-  def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this map (Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) Some(a) else None)
 
   def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
-
-  val absO: Option[Double] => Option[Double] = lift(math.abs)
-
-  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String) : Option[Double] = {
-    val optAge: Option[Int] = Try(age.toInt)
-    val optTickets: Option[Int] = Try(numberOfSpeedingTickets.toInt)
-    None
-  }
-  def Try[A](a: => A): Option[A] =
-    try Some(a)
-    catch { case e: Exception => None }
-
-  // Exercise 4.3
-//  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(aa => b.map(bb => f(aa, bb)))
 
   // Exercise 4.4
   def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
     case Nil => Some(Nil)
-    case h::t => h.flatMap(hh => sequence(t).map(hh :: _))
+    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
   }
-  // Exercise 4.5
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
-    case Nil => Some(Nil)
-    case h::t => map2(f(h), traverse(t)(f))(_::_)
-  }
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-    for {
-      aa <- a
-      bb <- b
-    } yield f(aa, bb)
 }
 
+case class Some[+A](get: A) extends Option[A]
 
-
+case object None extends Option[Nothing]
