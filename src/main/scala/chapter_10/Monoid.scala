d2d624df4026e8d2eeb48a8b0f3ac750408084a5
mjin <ming.jin@orbitz.com>
2018-05-24 14:51:07 -0500

    Started chapter 10

---
+++ src/main/scala/chapter_10/Monoid.scala
@@ -0,0 +1,57 @@
+package chapter_10
+
+trait Monoid[A] {
+  def op(a1: A, a2: A): A
+  def zero: A
+}
+
+object Monoid {
+  val stringMonoid = new Monoid[String] {
+    override def op(a1: String, a2: String): String = a1 + a2
+
+    override def zero: String = ""
+  }
+
+  def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
+    override def zero: List[A] = Nil
+  }
+
+  val intAddition = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 + a2
+
+    override def zero: Int = 0
+  }
+
+  val intMultiplication = new Monoid[Int] {
+    override def op(a1: Int, a2: Int): Int = a1 * a2
+
+    override def zero: Int = 1
+  }
+
+  val booleanOr = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+    override def zero: Boolean = false
+  }
+
+  val booleanAnd = new Monoid[Boolean] {
+    override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+    override def zero: Boolean = false
+  }
+
+  def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    def op(x: Option[A], y: Option[A]): Option[A] = x orElse y
+    val zero = None
+  }
+
+  def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    override def op(a1: A => A, a2: A => A): A => A = a1 compose a2
+
+    override def zero: A => A = (a: A) => a
+  }
+
+}
+
---
+++ src/test/scala/chapter_10/MonoidTest.scala
@@ -0,0 +1,6 @@
+package chapter_10
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class MonoidTest extends FlatSpec with Matchers {
+}
